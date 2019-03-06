01d3aed34e139902a6c426ab2892f2f7cc31e09f
tochwat <tochwat@gmail.com>
2019-03-05 21:19:25 -0800

    working initial checkItem function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,27 +21,56 @@ setTimeout(prompt,1000);
 
 function prompt() {
     inquirer
-      .prompt({
-        name: "itemID",
-        type: "input",
-        message: "What is the ID of the item you would like to purchase?"
-      },
-      {
-        name: "numUnits",
-        type: "input",
-        message: "How many units would you like?"
-      })
+      .prompt([
+        {
+            name: "itemID",
+            type: "input",
+            message: "What is the ID of the item you would like to purchase?"
+        },
+        {
+            name: "numUnits",
+            type: "input",
+            message: "How many units would you like?"
+        }
+      ])
       .then(function(answer) {
-        console.log(answer.itemID);
-        console.log(answer.numUnits);
-        connection.end();
+        // console.log(answer.itemID);
+        // console.log(answer.numUnits);
+        checkItem(answer.itemID, answer.numUnits);
       });
   }
 
 
 
-async function readAll() {
+function readAll() {
     connection.query('SELECT * FROM bamazon.products;', function (err, results, fields) {
         console.table(results);
+    });
+};
+
+//function to find the item in the DB and check if there's enough stock
+function checkItem(itemID, numUnits) {
+    // console.log("Running checkItem! Looking for item #: "+ itemID);
+    connection.query('SELECT * FROM bamazon.products;', function (err, results, fields) {
+        // get the information of the chosen item
+        var itemIdInt = parseInt(itemID);
+        var numUnitsInt = parseInt(numUnits);
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+            // console.log(results[i].item_id);
+            // console.log(itemIdInt);
+          if (results[i].item_id === itemIdInt) {
+            chosenItem = results[i];
+            console.table(chosenItem);
+          }
+        }
+
+        if (chosenItem.stock_quantity < numUnitsInt) {
+            console.log("Insufficient quantity in stock!")
+        } else {
+            console.log("Great, we'll get this order processed next!")
+        }
+
+        connection.end();
     });
 };
